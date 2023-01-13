5cde7b5d9229d15f33ef8be61d4a734356c7475f
Marco-Sorrentino <sorrentmarco@gmail.com>
2023-01-13 14:07:19 +0100

    js nav function done

---
+++ assets/script/script.js
@@ -0,0 +1,15 @@
+const className = "inverted";
+const scrollTrigger = 60;
+
+window.onscroll = function () {
+  // We add pageYOffset for compatibility with IE.
+  if (window.scrollY >= scrollTrigger || window.pageYOffset >= scrollTrigger) {
+    document.getElementsByTagName("header")[0].classList.add("active");
+    document.getElementById("ctaOne").classList.add("greenCta");
+  } else {
+    document.getElementsByTagName("header")[0].classList.remove("active");
+    document.getElementById("ctaOne").classList.remove("greenCta");
+  }
+};
+
+document.getElementById;
